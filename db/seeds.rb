8c96ea464abf36b9816927c049d13eb8f7c740f9
Benjamin Wendelstadt <bwendelstadt@gmail.com>
2019-11-21 12:00:38 +0200

    done backend of day 1 mst cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require 'json'
+require 'open-uri'
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+fetch = open(url).read
+data = JSON.parse(fetch)
+
+data['drinks'].each do |hash|
+  Ingredient.create(name: hash['strIngredient1'])
+end
